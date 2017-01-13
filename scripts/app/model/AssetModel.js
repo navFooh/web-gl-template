8f197fecf020c735b3d590ba8fca71600734580b
Thijs van Hoof <thijs.v.h@gmail.com>
2017-01-13 11:39:53 +0100

    assetmodel

---
+++ scripts/app/model/AssetModel.js
@@ -0,0 +1,65 @@
+define([
+	'backbone',
+	'three',
+	'three-OBJLoader'
+], function (Backbone, THREE) {
+
+	var onLoad = function() {
+			this.apply(this, arguments);
+			decrease();
+		},
+
+		onProgress = function(xhr) {
+			console.log(xhr.loaded, xhr.total)
+		},
+
+		onError = function(xhr) {
+			console.error(xhr);
+			decrease();
+		},
+
+		increase = function() {
+			assetModel.set({
+				loading: true,
+				total: assetModel.get('total') + 1
+			});
+		},
+
+		decrease = function() {
+			var total = assetModel.get('total'),
+				loaded = assetModel.get('loaded') + 1,
+				loading = loaded < total;
+			assetModel.set({
+				loading: loading,
+				loaded: loading ? loaded : 0,
+				total: loading ? total : 0
+			});
+		},
+
+		assetModel = new Backbone.Model.extend({
+
+			ANIMATION: { instance: null, CLASS: THREE.AnimationLoader },
+			AUDIO: { instance: null, CLASS: THREE.AudioLoader },
+			IMAGE: { instance: null, CLASS: THREE.ImageLoader },
+			JSON: { instance: null, CLASS: THREE.JSONLoader },
+			OBJ: { instance: null, CLASS: THREE.OBJLoader },
+			OBJECT: { instance: null, CLASS: THREE.ObjectLoader },
+			TEXTURE: { instance: null, CLASS: THREE.TextureLoader },
+			CUBE: { instance: null, CLASS: THREE.CubeTextureLoader },
+
+			defaults: {
+				loading: false,
+				loaded: 0,
+				total: 0
+			},
+
+			load: function(loader, url, callback) {
+				if (!loader.instance)
+					loader.instance = new loader.CLASS();
+				loader.instance.load(url, onLoad.bind(callback), onProgress, onError);
+				increase();
+			}
+		});
+
+	return assetModel;
+});
