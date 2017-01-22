9a11221c995c1726ee58353ee57855ba32323bf8
Thijs van Hoof <thijs.v.h@gmail.com>
2017-01-22 13:24:56 +0100

    orbit

--- scripts/app/util/Orbit.js
+++ scripts/app/util/Orbit.js
@@ -1,70 +1,53 @@
-define(['three'], function (THREE) {
+define([
+	'underscore',
+	'three'
+], function (_, THREE) {
 
-	return function (object, target) {
-
-		target = target || new THREE.Vector3();
-
-		this.minDistance = 0;
-		this.maxDistance = Infinity;
-		this.minPolarAngle = 0;
-		this.maxPolarAngle = Math.PI;
-		this.minAzimuthAngle = -Infinity;
-		this.maxAzimuthAngle = Infinity;
-
-		var scale = 1;
-		var offset = new THREE.Vector3();
-		var spherical = new THREE.Spherical();
-		var sphericalDelta = new THREE.Spherical();
-
-		// object.up is the orbit axis
-		var quat = new THREE.Quaternion().setFromUnitVectors(object.up, new THREE.Vector3(0, 1, 0));
-		var quatInverse = quat.clone().inverse();
-
-		this.getPolarAngle = function () {
-			return spherical.phi;
-		};
-
-		this.getAzimuthalAngle = function () {
-			return spherical.theta;
-		};
-
-		this.dollyIn = function (dollyScale) {
-			scale /= dollyScale;
-		};
-
-		this.dollyOut = function (dollyScale) {
-			scale *= dollyScale;
-		};
-
-		this.rotateLeft = function (angle) {
-			sphericalDelta.theta -= angle;
-		};
-
-		this.rotateUp = function (angle) {
-			sphericalDelta.phi -= angle;
-		};
-
-		this.update = function () {
-			offset.copy(object.position).sub(target);
-			offset.applyQuaternion(quat);
-
-			spherical.setFromVector3(offset);
-			spherical.theta += sphericalDelta.theta;
-			spherical.theta = Math.max(this.minAzimuthAngle, Math.min(this.maxAzimuthAngle, spherical.theta));
-			spherical.phi += sphericalDelta.phi;
-			spherical.phi = Math.max(this.minPolarAngle, Math.min(this.maxPolarAngle, spherical.phi));
-			spherical.radius *= scale;
-			spherical.radius = Math.max(this.minDistance, Math.min(this.maxDistance, spherical.radius));
-			spherical.makeSafe();
-
-			offset.setFromSpherical(spherical);
-			offset.applyQuaternion(quatInverse);
-
-			object.position.copy(target).add(offset);
-			object.lookAt(target);
-
-			scale = 1;
-			sphericalDelta.set(0, 0, 0);
-		};
+	var Orbit = function (object, target) {
+		this.object = object;
+		this.target = target instanceof THREE.Object3D ? target.position
+			: target instanceof THREE.Vector3 ? target : new THREE.Vector3();
+		this.spherical = new THREE.Spherical();
+		this.setSpherical();
 	};
+
+	_.extend(Orbit.prototype, {
+
+		setAxis: function() {
+			var up = new THREE.Vector3(0, 1, 0);
+
+			return function(axis) {
+				if (axis instanceof THREE.Vector3 && !axis.equals(up)) {
+					axis = axis.clone().normalize();
+					this.quat = new THREE.Quaternion().setFromUnitVectors(axis, up);
+					this.quatInverse = this.quat.clone().inverse();
+					this.rotated = true;
+				} else {
+					this.rotated = false;
+				}
+			}
+		}(),
+
+		setSpherical: function() {
+			var offset = new THREE.Vector3();
+
+			return function() {
+				offset.copy(this.object.position).sub(this.target);
+				this.spherical.setFromVector3(offset);
+			};
+		}(),
+
+		update: function () {
+			var offset = new THREE.Vector3();
+
+			return function() {
+				offset.setFromSpherical(this.spherical);
+				this.rotated && offset.applyQuaternion(this.quatInverse);
+				this.object.position.copy(this.target).add(offset);
+				this.object.lookAt(this.target);
+			};
+		}()
+	});
+
+	return Orbit;
 });
