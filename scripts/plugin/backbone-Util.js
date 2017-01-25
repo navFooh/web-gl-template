87748669cc6b75b4507c34aef19b17d006e0d991
Thijs van Hoof <thijs.v.h@gmail.com>
2017-01-25 23:00:06 +0100

    backbone util

---
+++ scripts/plugin/backbone-Util.js
@@ -0,0 +1,22 @@
+define(['backbone'], function(Backbone) {
+
+	var Util = Backbone.Util = function() {
+		this.cid = _.uniqueId('util');
+		this.initialize.apply(this, arguments);
+	};
+
+	_.extend(Util.prototype, Backbone.Events, {
+
+		initialize: function() {},
+
+		remove: function() {
+			this.stopListening();
+			return this;
+		}
+	});
+
+	Util.extend = Backbone.Model.extend;
+
+	return Util;
+
+});
