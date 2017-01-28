14c67a96c666534e8d2a542c27d976b4d70f183e
Thijs van Hoof <thijs.v.h@gmail.com>
2017-01-28 12:49:37 +0100

    add wheel

--- scripts/app/util/events/MouseEvents.js
+++ scripts/app/util/events/MouseEvents.js
@@ -1,11 +1,15 @@
-define(['backbone-Util'], function (Util) {
+define([
+	'backbone-Util',
+	'jquery-mousewheel'
+], function (Util) {
 
 	return Util.extend({
 
 		EVENT: {
 			DOWN: 0,
 			MOVE: 1,
-			UP: 2
+			UP: 2,
+			WHEEL: 3
 		},
 
 		initialize: function (element) {
@@ -15,6 +19,7 @@ define(['backbone-Util'], function (Util) {
 			this.element.addEventListener('mousemove', this.trigger.bind(this, this.EVENT.MOVE), true);
 			this.element.addEventListener('mouseup', this.trigger.bind(this, this.EVENT.UP), true);
 			this.element.addEventListener('mouseleave', this.trigger.bind(this, this.EVENT.UP), true);
+			$(this.element).on('mousewheel', this.trigger.bind(this, this.EVENT.WHEEL));
 		}
 	});
 });
