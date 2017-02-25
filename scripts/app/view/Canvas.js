95a0472162a2ac01bebada45a11a5d4843b4639d
Thijs van Hoof <thijs.v.h@gmail.com>
2017-02-25 12:58:13 +0100

    prevent zooming in chrome

--- scripts/app/view/Canvas.js
+++ scripts/app/view/Canvas.js
@@ -6,6 +6,12 @@ define(['backbone'], function(Backbone) {
 
 		render: function($parent) {
 			this.$el.appendTo($parent);
+
+			// prevent the zoom behaviour in Chrome
+			var noop = function(event) { event.preventDefault(); };
+			this.el.addEventListener('touchstart', noop);
+			this.el.addEventListener('touchmove', noop);
+
 			return this;
 		}
 	});
