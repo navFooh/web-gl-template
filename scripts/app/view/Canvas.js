152f3dd54a4355353bdf75a0349a2e2b4a3bfdc0
Thijs van Hoof <thijs.v.h@gmail.com>
2017-03-20 09:35:48 +0100

    set element for PointerModel

--- scripts/app/view/Canvas.js
+++ scripts/app/view/Canvas.js
@@ -1,8 +1,15 @@
-define(['backbone'], function(Backbone) {
+define([
+	'backbone',
+	'model/PointerModel'
+], function(Backbone, PointerModel) {
 
 	return Backbone.View.extend({
 
 		tagName: 'canvas',
+
+		initialize: function() {
+			PointerModel.set({ element: this.el });
+		},
 
 		render: function($parent) {
 			this.$el.appendTo($parent);
