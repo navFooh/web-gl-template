bffcc5504df458e4f83753caae601575a9793148
Thijs van Hoof <thijs.v.h@gmail.com>
2017-04-08 21:50:07 +0200

    store the font-size

--- scripts/app/model/DisplayModel.js
+++ scripts/app/model/DisplayModel.js
@@ -5,14 +5,24 @@ define(['backbone'], function (Backbone) {
 		defaults: {
 			width: 0,
 			height: 0,
-			aspect: 1
+			aspect: 1,
+			fontSize: 16
 		},
 
 		initialize: function () {
 			this.onResize();
+			this.setFontSize();
+
 			window.addEventListener('resize', this.onResize.bind(this));
+
 			this.on('resize', _.debounce(_.partial(this.trigger, 'resizeStart'), 250, true));
 			this.on('resize', _.debounce(_.partial(this.trigger, 'resizeEnd'), 250));
+		},
+
+		setFontSize: function() {
+			var style = window.getComputedStyle(document.body, null),
+				fontSize = style.getPropertyValue('font-size');
+			this.set({ fontSize: parseFloat(fontSize) || 16 });
 		},
 
 		onResize: function () {
