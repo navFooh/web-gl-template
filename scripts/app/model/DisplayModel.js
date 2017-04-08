2c08521864fe161c4866374c3866485d91869404
Thijs van Hoof <thijs.v.h@gmail.com>
2017-04-08 21:40:45 +0200

    fix indentation

--- scripts/app/model/DisplayModel.js
+++ scripts/app/model/DisplayModel.js
@@ -2,30 +2,30 @@ define(['backbone'], function (Backbone) {
 
 	var DisplayModel = Backbone.Model.extend({
 
-			defaults: {
-				width: 0,
-				height: 0,
-				aspect: 1
-			},
+		defaults: {
+			width: 0,
+			height: 0,
+			aspect: 1
+		},
 
-			initialize: function () {
-				this.onResize();
-				window.addEventListener('resize', this.onResize.bind(this));
-				this.on('resize', _.debounce(_.partial(this.trigger, 'resizeStart'), 250, true));
-				this.on('resize', _.debounce(_.partial(this.trigger, 'resizeEnd'), 250));
-			},
+		initialize: function () {
+			this.onResize();
+			window.addEventListener('resize', this.onResize.bind(this));
+			this.on('resize', _.debounce(_.partial(this.trigger, 'resizeStart'), 250, true));
+			this.on('resize', _.debounce(_.partial(this.trigger, 'resizeEnd'), 250));
+		},
 
-			onResize: function () {
-				var width = window.innerWidth,
-					height = window.innerHeight;
-				this.set({
-					width: width,
-					height: height,
-					aspect: width / height,
-					diagonal: Math.sqrt(width * width + height * height)
-				}).trigger('resize');
-			}
-		});
+		onResize: function () {
+			var width = window.innerWidth,
+				height = window.innerHeight;
+			this.set({
+				width: width,
+				height: height,
+				aspect: width / height,
+				diagonal: Math.sqrt(width * width + height * height)
+			}).trigger('resize');
+		}
+	});
 
 	return new DisplayModel();
 });
