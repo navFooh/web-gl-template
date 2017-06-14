37b568fe2958d144fee709d7ca5451691c28ebb9
Thijs van Hoof <thijs.v.h@gmail.com>
2017-06-14 19:07:32 +0200

    refactor cube

--- scripts/app/webgl/object/cube.js
+++ scripts/app/webgl/object/cube.js
@@ -1,20 +1,29 @@
 define([
 	'backbone-webgl',
 	'three',
-	'model/asset-model'
-], function (WebGL, THREE, AssetModel) {
+	'model/asset-model',
+	'model/webgl-model'
+], function (WebGL, THREE, AssetModel, WebGLModel) {
 
 	return WebGL.extend({
 
 		initialize: function () {
+			this.geometry = new THREE.BoxGeometry(100, 100, 100);
+			this.material = new THREE.MeshLambertMaterial();
+			this.mesh = new THREE.Mesh(this.geometry, this.material);
+			this.parent.add(this.mesh);
+
 			AssetModel.load(AssetModel.TEXTURE, 'img/og-image.jpg', this.onLoad.bind(this));
+
+			this.listenTo(WebGLModel, 'update', this.update);
 		},
 
 		onLoad: function (texture) {
-			this.parent.add(new THREE.Mesh(
-				new THREE.BoxGeometry(100, 100, 100),
-				new THREE.MeshLambertMaterial({ map: texture })
-			));
+			this.material.map = texture;
+		},
+
+		update: function (delta, elapsed) {
+			this.mesh.rotation.y = Math.sin(elapsed * 0.5) * Math.PI * 0.5;
 		}
 	});
 });
