4f06b99b1e87a57b5ce372b96e53a99703475963
Thijs van Hoof <thijs.v.h@gmail.com>
2017-06-16 15:35:24 +0200

    dots

--- scripts/app/view/preloader.js
+++ scripts/app/view/preloader.js
@@ -2,15 +2,26 @@ define([
 	'backbone',
 	'TweenMax',
 	'model/app-model',
+	'view/dots',
 	'templates/preloader'
-], function (Backbone, TweenMax, AppModel, template) {
+], function (Backbone, TweenMax, AppModel, Dots, template) {
 
 	return Backbone.View.extend({
 
 		render: function (parent) {
 			this.setElement(template(AppModel.toJSON()));
 			parent.appendChild(this.el);
+
+			var message = this.el.getElementsByClassName('message')[0];
+			this.dots = new Dots().render(message);
+
 			return this;
+		},
+
+		remove: function() {
+			this.dots.remove();
+			delete this.dots;
+			Backbone.View.prototype.remove.apply(this);
 		},
 
 		fadeOut: function (callback) {
