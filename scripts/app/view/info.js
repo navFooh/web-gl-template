1a5b23b25efb936537d33aefd11319a689e7ff4e
Thijs van Hoof <thijs.v.h@gmail.com>
2017-07-07 14:06:22 +0200

    fb version

--- scripts/app/view/info.js
+++ scripts/app/view/info.js
@@ -56,7 +56,7 @@ define([
 				var js, fjs = d.getElementsByTagName(s)[0];
 				if (d.getElementById(id)) return;
 				js = d.createElement(s); js.id = id;
-				js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&version=v2.8";
+				js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8";
 				fjs.parentNode.insertBefore(js, fjs);
 			}(document, 'script', 'facebook-jssdk'));
 		},
