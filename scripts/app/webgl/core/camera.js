8f45624318b0b138f94ab495376a167b628c4a5e
Thijs van Hoof <thijs.v.h@gmail.com>
2019-04-29 14:00:17 -0400

    camera fov fitting

--- scripts/app/webgl/core/camera.js
+++ scripts/app/webgl/core/camera.js
@@ -5,12 +5,28 @@ define([
 	'util/orbit-control'
 ], function (WebGL, THREE, DisplayModel, OrbitControl) {
 
+	var FIT_MODES = {
+		CONTAIN: 0,
+		COVER: 1
+	};
+
 	return WebGL.extend({
 
+		near: 1,
+		far: 5000,
+
+		defaultFOV: 50,
+
+		fitWidth: null,
+		fitHeight: null,
+		fitDepth: null,
+		fitMode: null,
+
 		initialize: function () {
+			var fov = this.getFOV();
 			var aspect = DisplayModel.get('aspect');
 
-			this.camera = new THREE.PerspectiveCamera(35, aspect, 1, 5000);
+			this.camera = new THREE.PerspectiveCamera(fov, aspect, this.near, this.far);
 			this.camera.position.set(0, 0, 500);
 			this.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
@@ -20,8 +36,26 @@ define([
 		},
 
 		onResize: function () {
+			this.camera.fov = this.getFOV();
 			this.camera.aspect = DisplayModel.get('aspect');
 			this.camera.updateProjectionMatrix();
+		},
+
+		getFOV: function () {
+			var aspect = DisplayModel.get('aspect');
+			var widthFOV = this.fitDepth && this.fitWidth ? 360 * Math.atan(this.fitWidth / aspect / (2 * this.fitDepth)) / Math.PI : null;
+			var heightFOV = this.fitDepth && this.fitHeight ? 360 * Math.atan(this.fitHeight / (2 * this.fitDepth)) / Math.PI : null;
+
+			if (widthFOV && heightFOV) {
+				switch (this.fitMode) {
+					case FIT_MODES.CONTAIN:
+						return Math.max(widthFOV, heightFOV);
+					case FIT_MODES.COVER:
+						return Math.min(widthFOV, heightFOV);
+				}
+			}
+
+			return widthFOV || heightFOV || this.defaultFOV;
 		}
 	});
 });
